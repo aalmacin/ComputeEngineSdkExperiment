ee8c77dfbf67531b1caec34678a1bd289139d08d
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2022-07-18 18:17:00 -0600

    Add instance group target size update

---
+++ src/main/java/UpdateInstanceGroupTargetCount.java
@@ -0,0 +1,48 @@
+import com.google.api.gax.longrunning.OperationFuture;
+import com.google.cloud.compute.v1.*;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class UpdateInstanceGroupTargetCount {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, TimeoutException {
+        String project = "raidrin-experiments";
+        String zone = "us-central1-a";
+        String instanceGroupName = "experiment-managed-instance-group";
+
+        try (InstanceGroupManagersClient instanceGroupManagersClient = InstanceGroupManagersClient.create()) {
+            GetInstanceGroupManagerRequest getInstanceGroupManagerRequest = GetInstanceGroupManagerRequest.newBuilder()
+                    .setInstanceGroupManager(instanceGroupName)
+                    .setZone(zone)
+                    .setProject(project)
+                    .build();
+            InstanceGroupManager instanceGroupManager = instanceGroupManagersClient.get(getInstanceGroupManagerRequest);
+            InstanceGroupManager updatedInstanceGroupManager = InstanceGroupManager.newBuilder()
+                    .setName(instanceGroupName)
+                    .setInstanceTemplateBytes(instanceGroupManager.getInstanceTemplateBytes())
+                    .setZone(zone)
+                    .setTargetSize(2)
+                    .build();
+            PatchInstanceGroupManagerRequest patchInstanceGroupManagerRequest = PatchInstanceGroupManagerRequest
+                    .newBuilder()
+                    .setInstanceGroupManager(instanceGroupName)
+                    .setInstanceGroupManagerResource(updatedInstanceGroupManager)
+                    .setProject(project)
+                    .setZone(zone)
+                    .build();
+            OperationFuture<Operation, Operation> operation =
+                    instanceGroupManagersClient.patchAsync(patchInstanceGroupManagerRequest);
+
+            // Wait for the operation to complete
+            Operation response = operation.get(5, TimeUnit.MINUTES);
+
+            if (response.hasError()) {
+                System.out.println("Instance creation failed !!" + response);
+                return;
+            }
+            System.out.println("Operation Status: " + response.getStatus());
+        }
+    }
+}
